e507fec3d46725bf38699f72244464a3753507d2
Aman Jain <aman@Amans-MacBook-Pro-3.local>
2025-04-28 16:28:33 -0700

    Implemented skill matching modules

---
+++ src/skill_matchmaker/config.py
@@ -0,0 +1,26 @@
+import os
+import yaml
+import importlib.resources
+
+# This is a configuration class for the Skill Matchmaker.
+# It loads configuration settings from a YAML file named "Config.yml".
+# The configuration settings include model names.
+
+
+class Config:
+    def __init__(self):
+        try:
+            # Correctly load config.yml from the installed package
+            configPath = importlib.resources.files('src.skill_matchmaker').joinpath('config.yml')
+            with configPath.open('r') as file:
+                config = yaml.safe_load(file)
+        except Exception as e:
+            raise RuntimeError(f"Failed to load config.yml: {e}")
+
+        # Set class attributes from config keys
+        self.TECHNICAL_MODEL_NAME_1 = config.get("TECHNICAL_MODEL_NAME_1")
+        self.TECHNICAL_MODEL_NAME_2 = config.get("TECHNICAL_MODEL_NAME_2")
+        self.TECHNICAL_MAX_INPUT_LENGTH = config.get("TECHNICAL_MAX_INPUT_LENGTH")
+        self.SOFT_MODEL_NAME_1 = config.get("SOFT_MODEL_NAME_1")
+        self.SOFT_MODEL_NAME_2 = config.get("SOFT_MODEL_NAME_2")
+        self.SOFT_MAX_INPUT_LENGTH = config.get("SOFT_MAX_INPUT_LENGTH")
