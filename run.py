351f93d4f8777d2cfbd15813b9839e69aaae81a3
Aman Jain <aman@Amans-MacBook-Pro-3.local>
2025-05-20 18:20:26 -0700

    Model preloading at startup

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 from api.app import create_app
+from sentence_transformers import SentenceTransformer
+
+print("⏳ Preloading Hugging Face models...")
+
+SentenceTransformer('all-mpnet-base-v2')
+SentenceTransformer('paraphrase-MiniLM-L3-v2')
+
+print("✅ Hugging Face models loaded and cached")
 
 app = create_app()
